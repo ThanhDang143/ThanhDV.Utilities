76178b782eb027c076cd57654eacb944622e33ba
ThanhDV <vanthanh1998@gmail.com>
2026-03-04 09:56:31 +0700

    Release v1.3.3
    Remove Singleton.OnAwake()

--- Assets/CustomPackages/Utilities/Singleton/Singleton.cs
+++ Assets/CustomPackages/Utilities/Singleton/Singleton.cs
@@ -60,7 +60,7 @@ namespace ThanhDV.Utilities
                             _instance = singletonObject.AddComponent<T>();
                             singletonObject.name = typeof(T).Name;
 
-                            Debug.Log($"<color=yellow>[Singleton] {typeof(T).Name} instance created (Lazy)!</color>");
+                            Debug.Log($"<color=yellow>[Singleton] {typeof(T).Name} instance created!</color>");
                         }
                     }
 
@@ -76,7 +76,6 @@ namespace ThanhDV.Utilities
             if (_instance == null)
             {
                 _instance = this as T;
-                OnAwake();
             }
             else if (_instance != this)
             {
@@ -84,8 +83,6 @@ namespace ThanhDV.Utilities
                 Destroy(gameObject);
             }
         }
-
-        protected virtual void OnAwake() { }
 
         protected virtual void OnApplicationQuit()
         {
@@ -104,12 +101,70 @@ namespace ThanhDV.Utilities
     /// <summary>
     /// A Singleton that persists across Scenes (DontDestroyOnLoad).
     /// </summary>
-    public class PersistentMonoSingleton<T> : MonoSingleton<T> where T : MonoBehaviour
+    public class PersistentMonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
     {
-        protected override void OnAwake()
+        private static T _instance;
+        private static readonly object _lock = new object();
+        private static bool _applicationIsQuitting = false;
+
+        public static T Instance
         {
-            base.OnAwake();
-            DontDestroyOnLoad(gameObject);
+            get
+            {
+                if (_applicationIsQuitting)
+                {
+                    Debug.Log($"<color=yellow>[Singleton] Instance '{typeof(T)}' already destroyed on application quit. Won't create again - returning null.");
+                    return null;
+                }
+
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = FindFirstObjectByType(typeof(T)) as T;
+
+                        if (_instance == null)
+                        {
+                            GameObject singletonObject = new GameObject();
+                            _instance = singletonObject.AddComponent<T>();
+                            singletonObject.name = typeof(T).Name;
+
+                            Debug.Log($"<color=yellow>[Singleton] {typeof(T).Name} instance created!</color>");
+                        }
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+
+        public static bool IsExist => _instance != null;
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (_instance != this)
+            {
+                Debug.Log($"<color=yellow>[Singleton] Another instance of {typeof(T)} detected! Destroying new one.");
+                Destroy(gameObject);
+            }
+        }
+
+        protected virtual void OnApplicationQuit()
+        {
+            _applicationIsQuitting = true;
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (_instance == this)
+            {
+                _applicationIsQuitting = true;
+            }
         }
     }
 }
